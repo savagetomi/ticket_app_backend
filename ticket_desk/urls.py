3b78880863c5486e66404d18131d81713bc000ee
Epaphras Savage <dhareykhaey3@gmail.com>
2026-08-12 20:54:53 +0100

    Trying to view images

--- ticket_desk/urls.py
+++ ticket_desk/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import static
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
 from drf_spectacular.views import (
     SpectacularAPIView,
@@ -23,7 +24,6 @@ from drf_spectacular.views import (
     SpectacularRedocView,
 )
 
-from ticket_desk import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -50,7 +50,9 @@ urlpatterns = [
 ]
 
 
-urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
-)
+# Serve uploaded media files during development
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
